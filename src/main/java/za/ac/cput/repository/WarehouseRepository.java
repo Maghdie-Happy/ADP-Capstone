55370159b10c52b7c4f9c58ed6cc56042aa74182
Tebogo <notorioustee11@gmail.com>
2026-03-24 18:05:34 +0200

    Repository Commit. Need CustomerFactory for tests(230226442)

--- src/main/java/za/ac/cput/repository/WarehouseRepository.java
+++ src/main/java/za/ac/cput/repository/WarehouseRepository.java
@@ -1,4 +1,66 @@
 package za.ac.cput.repository;
 
-public class WarehouseRepository {
+import za.ac.cput.domain.Warehouse;
+import java.util.*;
+
+public class WarehouseRepository implements IWarehouseRepository {
+
+    private static WarehouseRepository repository = null;
+    private Set<Warehouse> warehouseSet = null;
+
+    private WarehouseRepository() {
+        warehouseSet = new HashSet<>();
+    }
+
+    public static WarehouseRepository getRepository() {
+        if (repository == null) {
+            repository = new WarehouseRepository();
+        }
+        return repository;
+    }
+
+    @Override
+    public Warehouse create(Warehouse warehouse) {
+        boolean success = warehouseSet.add(warehouse);
+        if (!success) {
+            return null;
+        }
+        return warehouse;
+    }
+
+    @Override
+    public Warehouse read(String warehouseId) {
+        for (Warehouse w : warehouseSet) {
+            if (w.getWarehouseId().equals(warehouseId)) {
+                return w;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Warehouse update(Warehouse warehouse) {
+        Warehouse oldWarehouse = read(warehouse.getWarehouseId());
+        if (oldWarehouse != null) {
+            warehouseSet.remove(oldWarehouse);
+            warehouseSet.add(warehouse);
+            return warehouse;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean delete(String warehouseId) {
+        Warehouse warehouseToDelete = read(warehouseId);
+        if (warehouseToDelete == null) {
+            return false;
+        }
+        warehouseSet.remove(warehouseToDelete);
+        return true;
+    }
+
+    @Override
+    public Set<Warehouse> getAll() {
+        return warehouseSet;
+    }
 }
